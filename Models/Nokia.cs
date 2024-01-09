39ed8772d5ae42a8e1dd5aa20f484c663d186434
Gabriela Pimenta <gabrielapimenta_@hotmail.com>
2024-01-09 20:21:20 -0300

    Finalizado

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,15 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    // Herança da classe "Smartphone"
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Nokia(string numero, string modelo, string imei, int memoria) : base (numero, modelo, imei, memoria)
+        {
+        }
+        // Sobrescrevendo o método "InstalarAplicativo
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"O aplicativo {nomeApp} foi instalado no Nokia com sucesso.");
+        }
     }
 }

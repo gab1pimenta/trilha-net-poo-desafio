04ee014a8c2f311a2cc7ca54422615bc54fd41f6
Gabriela Pimenta <gabrielapimenta_@hotmail.com>
2024-01-09 20:20:03 -0300

    Finalizado

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,16 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    // Herança da classe "Smartphone"
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Iphone(string numero, string modelo, string imei, int memoria) : base (numero, modelo, imei, memoria)
+        {
+        }
+        //Sobrescrevendo o método "InstalarAplicativo
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"O aplicativo {nomeApp} foi instalado no Iphone com sucesso.");
+        }
+
     }
 }
